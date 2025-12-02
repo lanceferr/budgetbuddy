f8e1161cdc384f9075af30f74c579868b441726f
lanceferr <lanceferrer1122@gmail.com>
2025-12-02 19:38:04 +0800

    feat: added router post for tracking

--- backend/src/router/savings.ts
+++ backend/src/router/savings.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import { addSavingsGoal, listSavingsGoals, listSavingsGoalById, updateSavingsGoal, deleteSavingsGoal} from  '../controllers/savings.ts';
+import { addSavingsGoal, listSavingsGoals, listSavingsGoalById, updateSavingsGoal, deleteSavingsGoal, addSavingsContribution} from  '../controllers/savings.ts';
 import { isAuthenticated } from '../middlewares/index.ts';  
 
 export default (router: express.Router) => {
     router.get('/savings-goals', isAuthenticated, listSavingsGoals);
     router.post('/savings-goals', isAuthenticated, addSavingsGoal);
-    router.get('/savings-goals/:id', isAuthenticated, listSavingsGoalById);
-    router.patch('/savings-goals/:id', isAuthenticated, updateSavingsGoal);
-    router.delete('/savings-goals/:id', isAuthenticated, deleteSavingsGoal);
+    router.post('savings-goals/:goalId/contribute', isAuthenticated, addSavingsContribution);
+    router.get('/savings-goals/:goalId', isAuthenticated, listSavingsGoalById);
+    router.patch('/savings-goals/:goalId', isAuthenticated, updateSavingsGoal);
+    router.delete('/savings-goals/:goalId', isAuthenticated, deleteSavingsGoal);
 };
